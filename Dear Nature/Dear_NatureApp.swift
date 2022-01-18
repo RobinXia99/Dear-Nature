3ca4f71c9ace9141f8674cb1df827fcab5d71dfe
RobinXia99 <>
2022-01-18 11:47:24 +0100

    Initial commit

---
+++ Dear Nature/Dear_NatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dear_NatureApp.swift
+//  Dear Nature
+//
+//  Created by Robin Xia on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Dear_NatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
